04e7dcefdbe4b3e22316f629956362d6d37a8b49
anionos <aniebietonoyom32@gmail.com>
2018-09-25 15:43:03 +0100

    implementing Fragments

---
+++ app/src/main/java/com/example/android/miwok/ColorsFragment.java
@@ -0,0 +1,14 @@
+package com.example.android.miwok;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by user on 9/25/2018.
+ */
+
+public class ColorsFragment extends Fragment {
+    public ColorsFragment(){
+
+    }
+
+}
---
+++ app/src/main/java/com/example/android/miwok/PhrasesFragment.java
@@ -0,0 +1,13 @@
+package com.example.android.miwok;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by user on 9/25/2018.
+ */
+
+public class PhrasesFragment extends Fragment {
+    public PhrasesFragment(){
+
+    }
+}
